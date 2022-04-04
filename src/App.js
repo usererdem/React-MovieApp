47f1b1b621d8c3978d27c0ebf8a572dfbf8eca46
Erdem Elvan <erdemelvan35@gmail.com>
2022-04-04 21:39:02 +0300

    movieapp tutorial finished

--- src/App.js
+++ src/App.js
@@ -9,20 +9,46 @@ const SEARCH_API = "https://api.themoviedb.org/3/search/movie?&api_key=04c35731a
 
 function App() {
   const [movies, setMovies] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
 
   useEffect(() => {
-    fetch(FEATURED_API)
+    getMovies(FEATURED_API);
+  }, []);
+
+  const getMovies = (API) => {
+    fetch(API)
       .then((res) => res.json())
       .then((data) => {
         console.log(data);
         setMovies(data.results);
       });
-  }, []);
+  }
 
+  const handleOnSubmit = (e) => {
+    e.preventDefault();
+
+    if (searchTerm) {
+      getMovies(SEARCH_API+ searchTerm);
+
+          setSearchTerm("");
+    }
+  };
+
+  const handleOnChange = (e) => {
+    setSearchTerm(e.target.value);
+  };
   return (
     <>
       <header>
-        <input className="search" type="text" placeholder="Search..."></input>
+        <form onSubmit={handleOnSubmit}>
+          <input
+            className="search"
+            type="text"
+            placeholder="Search..."
+            value={searchTerm}
+            onChange={handleOnChange}
+          ></input>
+        </form>
       </header>
       <div className="movie-container">
         {movies.length > 0 && movies.map((movie) => <Movie key={movie.id} {...movie} />)}
--- src/components/Movie.js
+++ src/components/Movie.js
@@ -2,12 +2,22 @@ import React from "react";
 
 const IMG_API = "https://image.tmdb.org/t/p/w1280";
 
+const setVoteClass = (vote) => {
+  if(vote >= 8) {
+    return 'green'
+  } else if (vote >= 6) {
+    return 'orange';
+  } else {
+    return 'red';
+  }
+}
+
 const Movie = ({ title, poster_path, overview, vote_average }) => (
   <div className="movie">
-    <img src={IMG_API + poster_path} alt={title}></img>
+    <img src={poster_path ? (IMG_API + poster_path): 'https://images.unsplash.com/photo-1485846234645-a62644f84728?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1159&q=80'} alt={title}></img>
     <div className="movie-info">
       <h3>{title}</h3>
-      <span>{vote_average}</span>
+      <span className={`tag ${setVoteClass(vote_average)}`}>{vote_average}</span>
     </div>
 
     <div className="movie-over">
